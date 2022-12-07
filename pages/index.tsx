bf43978516d628506f4eb965a9f0da22806cf40a
Mateo Molfino ximenez <mateomolfinoximenez@192.168.1.6>
2022-12-07 17:54:00 -0300

    delete exp

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,26 +39,6 @@ const Home = ({ randomImage, rickAndMorty
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-
-  // const [
-  //   netflixOriginals,
-  //   trendingNow,
-  //   topRated,
-  //   actionMovies,
-  //   comedyMovies,
-  //   horrorMovies,
-  //   romanceMovies,
-  //   documentaries,
-  // ] = await Promise.all([
-  //   fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
-  //   fetch(requests.fetchTrending).then((res) => res.json()),
-  //   fetch(requests.fetchTopRated).then((res) => res.json()),
-  //   fetch(requests.fetchActionMovies).then((res) => res.json()),
-  //   fetch(requests.fetchComedyMovies).then((res) => res.json()),
-  //   fetch(requests.fetchHorrorMovies).then((res) => res.json()),
-  //   fetch(requests.fetchRomanceMovies).then((res) => res.json()),
-  //   fetch(requests.fetchDocumentaries).then((res) => res.json()),
-  // ])
   const [
     randomImage,
     rickAndMorty
