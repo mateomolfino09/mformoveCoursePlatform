e179c27ae2a90b5d4c94d73297176b7ce5c66cad
Mateo Molfino ximenez <mateomolfinoximenez@192.168.1.3>
2022-12-24 17:40:26 -0300

    corr5

--- pages/cursos/[id].tsx
+++ pages/cursos/[id].tsx
@@ -17,7 +17,7 @@ function Curso({ curso, playlist }: Props) {
   return (
     <div>
        <h1>Curso 1</h1> 
-        <Youtube playlist = {playlist?.items}/>
+        {playlist.items && <Youtube playlist = {playlist.items}/>}
     </div>
   )
 }
@@ -32,7 +32,7 @@ export const getStaticPaths = async () => {
     return { paths, fallback: false }
 }
 
-export const getServerSideProps = async (context: any) => {
+export const getStaticProps = async (context: any) => {
     const id = context.params.id;
 
     const res = await fetch(requests.fetchRickAndmort + '/' + id)
