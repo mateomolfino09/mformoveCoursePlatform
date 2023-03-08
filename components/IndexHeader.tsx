9ee38016bb6cac39e38468d627212445037ad9f3
mateomolfino09 <mateomolfino09@gmail.com>
2023-03-07 21:18:26 -0300

    arreglado

--- components/IndexHeader.tsx
+++ components/IndexHeader.tsx
@@ -33,8 +33,8 @@ return (
         <img
               alt='icon image'
               src="https://rb.gy/ulxxee"
-              width={100}
-              height={100}
+              width={120}
+              height={120}
               className="cursor-pointer object-contain"
             />
         </div>
--- pages/src/admin/users.tsx
+++ pages/src/admin/users.tsx
@@ -51,7 +51,6 @@ const ShowUsers = ({ users, user }: Props) => {
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
-<<<<<<< HEAD
       <div className="px-4 py-4 lg:px-10 lg:py-6">
         <h1 className="text-2xl mb-8">Usuarios</h1>
         <table className="min-w-full text-sm  ">
@@ -64,66 +63,6 @@ const ShowUsers = ({ users, user }: Props) => {
               <th className="border  text-xl">Actions</th>
             </tr>
           </thead>
-          <tbody>
-            {users?.map((user: any) => (
-              <tr key={user._id}>
-                <th ref={ref} className="border text-base  opacity-75">
-                  {user.name}
-                </th>
-                <th className="border text-base  opacity-75">{user.email}</th>
-                <th className="border text-base  opacity-75">{user.rol}</th>
-                <th className="border text-base  opacity-75">
-                  {new Date(user.createdAt).toLocaleDateString("es-ES")}
-                </th>
-                <th className=" border text-base py-3 px-6 text-center  ">
-                  <div className="flex item-center justify-center">
-                    <div className="opacity-75 w-6 mr-2 transform hover:text-blue-500 hover:scale-110">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-                        />
-                      </svg>
-                    </div>
-                    <div className="opacity-75 w-6 mr-2 transform hover:text-red-500 hover:scale-110">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        onClick={openModal}
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                        />
-                      </svg>
-                    </div>
-                  </div>
-                </th>
-=======
-        <div className="px-4 py-4 lg:px-10 lg:py-6 w-full">
-          <h1 className="text-transparent text-3xl my-12 font-bold">Usuarios</h1>
-          <table className="min-w-full text-sm border-solid border-transparent border border-collapse ">
-            <thead>
-              <tr>
-                <th className="border-solid border-2 border-transparent border-collapse bg-light-red-darker text-xl ">Nombre</th>
-                <th className="border-solid border-transparent border-2 border-collapse bg-light-red-darker text-xl">Email</th>
-                <th className="border-solid border-transparent border-2 border-collapse bg-light-red-darker text-xl">Rol</th>
-                <th className="border-solid border-transparent border-2 border-collapse  bg-light-red-darker text-xl">Fecha Creación</th>
-                <th className="border-solid border-transparent border-2 border-collapse bg-light-red-darker text-xl">Actions</th>
->>>>>>> a59710e6ece7af4cae7d7fd8fbc22d833036e6fe
-              </tr>
-            </thead>
             <tbody>
               {users?.map((user: any) => (
                 <tr key={user._id}>
