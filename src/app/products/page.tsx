d973bef78797add904a2c85c0edaf548e92f34a3
mateomolfino09 <mateomolfino09@gmail.com>
2024-04-17 15:46:38 -0300

    dix

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,11 +1,11 @@
 import Products from '../../components/PageComponent/Products/Products';
-import { getProductFilters } from '../api/workShop/getFilters';
-import { getWorkShops } from '../api/workShop/getWorkShops';
+import { getProductFilters } from '../api/product/getFilters';
+import { getProducts } from '../api/product/getProducts';
   
   
   export default async function Page() {
 
-    const workshops = await getWorkShops()
+    const workshops = await getProducts()
     const filters = await getProductFilters();
 
   
