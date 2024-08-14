c7a72efe2c97cd2f92dac1488f4354af829739e4
mateomolfino09 <mateomolfino09@gmail.com>
2024-08-14 10:05:27 -0300

    finished front

--- src/components/MainSideBarProducts/FreeProductCountDown.jsx
+++ src/components/MainSideBarProducts/FreeProductCountDown.jsx
@@ -54,14 +54,11 @@ const Timer2 = () => {
     startCountDown();
   }, [startCountDown]);
   return (
-      <div className="mx-3 sm:p-10 p-4 rounded-md flex justify-center flex-col gap-6 shadow-[5px_5px_50px_rgba(47,46,60,1)] font-montserrat">
+      <div className="mx-3 mt-12 sm:p-10 p-4 rounded-md flex justify-center flex-col gap-6 shadow-[5px_5px_50px_rgba(47,46,60,1)] font-montserrat">
         <div className="flex flex-col gap-2">
-          <h1 className="text-center sm:text-3xl text-xl font-semibold leading-8 text-[#141414]">
-            Apurate, Disponibilidad Limitada
+          <h1 className="text-center sm:text-xl text-lg font-semibold leading-8 text-[#141414]">
+            Esta oferta termina en...
           </h1>
-          <span className="text-sm font-semibold capitalize text-center leading-8 text-[#959AAE]">
-            Se parte de nuestra comunidad, aprovecha esta oportunidad antes de que se termine!
-          </span>
         </div>
         <div className="flex space-x-2 justify-center sm:px-4">
           <div className="flex flex-col justify-center items-center">
