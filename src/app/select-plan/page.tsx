f2728d04bd4a53e9f0274f057a90e3ac6f2ef615
mateomolfino09 <mateomolfino09@gmail.com>
2024-09-09 12:38:48 -0300

    origin error

--- src/app/select-plan/page.tsx
+++ src/app/select-plan/page.tsx
@@ -5,10 +5,14 @@ import { getPlans } from '../api/payments/getPlans';
   export default async function Page() {
 
     const plans = await getPlans()
+    if (process.env.NODE_ENV === 'development') {
+      origin = "https://checkout-sbx.dlocalgo.com"
+    } else {
+      origin = "https://checkout.dlocalgo.com"
+    }
 
-  
     return (
-      <SelectPlan plans={plans} />
+      <SelectPlan plans={plans} origin={origin}/>
     );
   };
   
