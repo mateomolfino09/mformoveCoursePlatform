41419f5043f51e91275c79cdcc7c1aee54aa6193
indutoptecnologia <tecnologia@indutop.uy>
2024-10-15 22:22:04 -0300

    prueba

--- src/app/admin/memberships/plans/page.tsx
+++ src/app/admin/memberships/plans/page.tsx
@@ -6,12 +6,11 @@ export const fetchCache = "force-no-store";
 
 export default async function Page() {
   let plans: Plan[] = [];
-  
   const baseUrl = process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000'
-    : process.env.NEXT_PUBLIC_BASE_URL;
+  ? 'http://localhost:3000'
+  : process.env.NEXT_PUBLIC_BASE_URL || `https://${process.env.VERCEL_URL}`;
 
-  const res = await fetch(`${baseUrl}/api/payments/plans`, { next: { revalidate: 0 } });
+const res = await fetch(`${baseUrl}/api/payments/plans`, { next: { revalidate: 0 } });
   
   if (!res.ok) {
     throw new Error('Failed to fetch plans');
--- src/app/select-plan/page.tsx
+++ src/app/select-plan/page.tsx
@@ -3,8 +3,11 @@ import SelectPlan from '../../components/PageComponent/SelectPlan/SelectPlan';
 export default async function Page() {
   let plans = [];
 
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/payments/plans`, { next: { revalidate: 0 } });
+  const baseUrl = process.env.NODE_ENV === 'development'
+  ? 'http://localhost:3000'
+  : process.env.NEXT_PUBLIC_BASE_URL || `https://${process.env.VERCEL_URL}`;
 
+const res = await fetch(`${baseUrl}/api/payments/plans`, { next: { revalidate: 0 } });
 
   if (!res.ok) {
     throw new Error('Failed to fetch plans');
