b2ef4094c99a9dd3092fbf6eb8213e2a5ff970f0
indutoptecnologia <tecnologia@indutop.uy>
2024-10-15 22:56:02 -0300

    .

--- src/app/api/payments/plans/route.js
+++ src/app/api/payments/plans/route.js
@@ -1,23 +1,20 @@
-import connectDB from '../../../../config/connectDB';
-import Plan from '../../../../models/planModel';
-import Course from '../../../../models/courseModel';
-import User from '../../../../models/userModel';
+import connectDB from '../../../../../config/connectDB';
+import Plan from '../../../../../models/planModel';
+import Course from '../../../../../models/courseModel';
+import User from '../../../../../models/userModel';
 
 connectDB();
 
-export const dynamic = "force-dynamic"; 
-export const fetchCache = "force-no-store"
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
 
-export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    try {
-      const plans = await Plan.find({});
-      res.status(200).json(plans);
-    } catch (error) {
-      console.error("Error fetching plans:", error);
-      res.status(500).json({ message: 'Error fetching plans' });
-    }
-  } else {
-    res.status(405).json({ message: 'Method Not Allowed' });
+// Cambia "handler" por "GET" para manejar solicitudes GET
+export async function GET(req) {
+  try {
+    const plans = await Plan.find({});
+    return new Response(JSON.stringify(plans), { status: 200 });
+  } catch (error) {
+    console.error("Error fetching plans:", error);
+    return new Response(JSON.stringify({ message: 'Error fetching plans' }), { status: 500 });
   }
 }
