27df58fb8bbc5c222936f8f48aa8404a4c0dd1a4
indutoptecnologia <tecnologia@indutop.uy>
2024-10-15 23:12:38 -0300

    .

--- src/app/api/payments/plans/route.js
+++ src/app/api/payments/plans/route.js
@@ -1,18 +1,21 @@
-
-import Plan from '../../../../models/planModel';
-
-import connectDB from '../../../../config/connectDB';
+import connectDB from '../../../../../config/connectDB';
+import Plan from '../../../../../models/planModel';
 
 connectDB();
 
 export const dynamic = 'force-dynamic';
 export const fetchCache = 'force-no-store';
 
-// Cambia "handler" por "GET" para manejar solicitudes GET
 export async function GET(req) {
   try {
     const plans = await Plan.find({});
-    return new Response(JSON.stringify(plans), { status: 200 });
+    return new Response(JSON.stringify(plans), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+    });
   } catch (error) {
     console.error("Error fetching plans:", error);
     return new Response(JSON.stringify({ message: 'Error fetching plans' }), { status: 500 });
