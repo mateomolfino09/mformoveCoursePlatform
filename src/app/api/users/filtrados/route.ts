f89b497f6cad06636b2756623c2b7cc3bd794007
nicolas-mauber <nicolasmauber2002@gmail.com>
2025-01-24 21:15:02 -0300

    Se agrega filtro en admin dashboard para seleccionar vip o no vip usuario

--- src/app/api/users/filtrados/route.ts
+++ src/app/api/users/filtrados/route.ts
@@ -1,6 +1,6 @@
+import connectDB from '../../../../config/connectDB';
+import User from '../../../../models/userModel';
 import { NextResponse } from 'next/server';
-import connectDB from '../../../config/connectDB';
-import User from '../../../models/userModel';
 
 connectDB();
 
@@ -8,33 +8,26 @@ export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const page = parseInt(searchParams.get('page') || '1', 10);
   const limit = parseInt(searchParams.get('limit') || '10', 10);
-  const filter = searchParams.get('filter') || 'ALL';
+  const isVipParam = searchParams.get('isVip'); // Original string value
+  const isVip = isVipParam === 'true'; // Convert to boolean
 
   const skip = (page - 1) * limit;
 
   try {
-    let query = {};
-
-    if (filter === 'VIP') {
-      query = { subscription: { $exists: true, $ne: null } };
-    } else if (filter === 'NON_VIP') {
-      query = { subscription: { $exists: false } };
-    }
-
-    const users = await User.find(query).skip(skip).limit(limit).lean();
+    const users = await User.find({ isVip }).skip(skip).limit(limit).lean();
 
     const serializedUsers = users.map((user) => ({
       ...user,
-      _id: user._id.toString(),
+      _id: user._id.toString()
     }));
 
-    const total = await User.countDocuments(query);
+    const total = await User.countDocuments({ isVip }); // Count only matching documents
 
     return NextResponse.json({
       users: serializedUsers,
       total,
       totalPages: Math.ceil(total / limit),
-      currentPage: page,
+      currentPage: page
     });
   } catch (error) {
     console.error('Error al cargar usuarios:', error);
